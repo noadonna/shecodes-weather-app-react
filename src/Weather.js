2c1955aa3a62ec3fa75ae4bd81aa35ecfabb6b62
noadonna <noadonna@users.noreply.github.com>
2020-11-19 11:16:25 +0100

    Imported React project

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 export default function Weather() {
-    function displayWeather(response) {
-    alert(`The weather in city is ${response.data.main.temp}°C`);
-    }
+  let [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function handleSubmit(event) {
+    event.preventDefault();
     let apiKey = "c21c69cfcac20320f0c03c2e080dbc19";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tilburg&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeather);
-    return <h2>Weather App</h2>
+  }
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description
+    });
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        onChange={updateCity}
+        placeholder="Enter a city..."
+      />
+      <button type="Submit">Search</button>
+    </form>
+  );
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  if (loaded) {
+    return (
+      <div className="Weather">
+        {form}
+        <ul className="condition">
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
